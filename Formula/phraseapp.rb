20fe41d247db13fa93ff9c93f502656563e28207
Frederik Vollert <frederik.vollert@dynport.de>
2015-09-24 17:55:44 +0200

    Update to 1.1.0
    Adds webhooks endpoint

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -5,9 +5,9 @@
 class Phraseapp < Formula
   desc "PhraseApp CLI Tool - API client binary"
   homepage "https://phraseapp.com/cli"
-  url "https://github.com/phrase/phraseapp-client/releases/download/1.0.2/phraseapp_macosx_amd64.tar.gz"
-  sha256 "dcf8fda495f639ee30217ba913ae8ce6d5ebaea69760047afcac5a0b09406999"
-  version "1.0.2"
+  url "https://github.com/phrase/phraseapp-client/releases/download/1.1.0/phraseapp_macosx_amd64.tar.gz"
+  sha256 "3bb48502e31f4cf0260b22942db2b476851d7ca6996c6cbca5ca7a27e8b4a99f"
+  version "1.1.0"
 
   def install
     bin.install "phraseapp"
