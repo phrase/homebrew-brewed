c28e2283ea8fcc8612e175afe27f856ff2e48367
fredv <f@dynport.de>
2015-10-13 10:49:50 +0200

    updates to 1.1.2 also in link

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -5,7 +5,7 @@
 class Phraseapp < Formula
   desc "PhraseApp CLI Tool - API client binary"
   homepage "https://phraseapp.com/cli"
-  url "https://github.com/phrase/phraseapp-client/releases/download/1.1.1/phraseapp_macosx_amd64.tar.gz"
+  url "https://github.com/phrase/phraseapp-client/releases/download/1.1.2/phraseapp_macosx_amd64.tar.gz"
   sha256 "cfb2b35499798eec2d3e7ca9bfb81d51ff7ce6cca14de63056efcc834f591c33"
   version "1.1.2"
 
