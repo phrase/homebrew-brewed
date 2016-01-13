2bc199106ce12b9b086df035df49b1f97577aa96
Gereon Frey <gereon.frey@dynport.de>
2016-01-13 08:47:25 +0100

    improved the formula
    It's no longer necessary to set the version twice.

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -5,9 +5,11 @@
 class Phraseapp < Formula
   desc "PhraseApp CLI Tool - API client binary"
   homepage "https://phraseapp.com/cli"
-  url "https://github.com/phrase/phraseapp-client/releases/download/1.1.8/phraseapp_macosx_amd64.tar.gz"
-  sha256 "3d951bff2e7090e28b65d4d52ef8b5b3e111aee74d2a9954d26a2761ba8aee28"
+
   version "1.1.8"
+  sha256 "3d951bff2e7090e28b65d4d52ef8b5b3e111aee74d2a9954d26a2761ba8aee28"
+
+  url "https://github.com/phrase/phraseapp-client/releases/download/#{version}/phraseapp_macosx_amd64.tar.gz"
 
   def install
     bin.install "phraseapp"
