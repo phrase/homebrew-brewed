4bd3bee0a5f4a6dea0d37ffd16a708acb7c7add2
sacry-dyn <matthias.nitsche@dynport.de>
2016-06-22 09:56:37 +0200

    version bump to 1.1.18

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -6,8 +6,8 @@ class Phraseapp < Formula
   desc "PhraseApp CLI Tool - API client binary"
   homepage "https://phraseapp.com/cli"
 
-  version "1.1.17"
-  sha256 "7e9086c277e2e115025b38507c3556d4f0a7a7535dd28c6132114b3b1195f7e4"
+  version "1.1.18"
+  sha256 "18fea41fe95bea0c2f9fc962fb63f1cbd1f2591538968528dc1492e4166b85e2"
 
   url "https://github.com/phrase/phraseapp-client/releases/download/#{version}/phraseapp_macosx_amd64.tar.gz"
 
