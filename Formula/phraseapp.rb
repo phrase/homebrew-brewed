b6fee3116edeaae57009a4f8eab9734fbc6cbd06
kirchner <kirchner@posteo.de>
2018-06-19 10:56:52 +0200

    version bump to 1.7.0

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -6,8 +6,8 @@ class Phraseapp < Formula
   desc "PhraseApp CLI Tool - API client binary"
   homepage "https://phraseapp.com/cli"
 
-  version "1.6.1"
-  sha256 "c25309d792917bb28628b79d1d5f7c14763c14fed9d9fc5bcbc9d98d3fb31e66"
+  version "1.7.0"
+  sha256 "66a723cdb45a498ea073e2e3c08ff072f72699a39259310ba7e2befdcb8e2665"
 
   url "https://github.com/phrase/phraseapp-client/releases/download/#{version}/phraseapp_macosx_amd64.tar.gz"
 
