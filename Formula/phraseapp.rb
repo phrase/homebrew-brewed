dd22d3cc7cbcecf15945cfc3a0f1bbec034e76c3
Tim Leers <tim.leers@phraseapp.com>
2018-08-23 11:32:44 +0200

    update to 1.9.1

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -6,8 +6,8 @@ class Phraseapp < Formula
   desc "PhraseApp CLI Tool - API client binary"
   homepage "https://phraseapp.com/cli"
 
-  version "1.9.0"
-  sha256 "922477b43789a8f55a73c7971bab191273461223087a2763346181bf107e5ba9"
+  version "1.9.1"
+  sha256 "3aa3a231ce62497b5c342df9be238a94418a0f48e0b8cba48ce4a13d02151ced"
 
   url "https://github.com/phrase/phraseapp-client/releases/download/#{version}/phraseapp_macosx_amd64.tar.gz"
 
