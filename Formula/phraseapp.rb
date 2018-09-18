1ca4abc17dc4b21d8a0f736fa08f9b0f05521454
Sönke Behrendt <soenke.behrendt@gmail.com>
2018-09-18 14:58:47 +0200

    Fix version number

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -6,7 +6,7 @@ class Phraseapp < Formula
   desc "PhraseApp CLI Tool - API client binary"
   homepage "https://phraseapp.com/cli"
 
-  version "1.10.0"
+  version "1.11.0"
   sha256 "4fb4468afd852e9880d2fa76cd4d5864efc742ddd780aa7b2b954a3b54360073"
 
   url "https://github.com/phrase/phraseapp-client/releases/download/#{version}/phraseapp_macosx_amd64.tar.gz"
