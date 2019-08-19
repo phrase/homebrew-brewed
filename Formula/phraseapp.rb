9a667b58bbb6f50614137d7347ce02f4da7f8c91
Sönke Behrendt <soenke.behrendt@gmail.com>
2019-08-19 11:22:06 +0200

    Update to 1.15.1

--- Formula/phraseapp.rb
+++ Formula/phraseapp.rb
@@ -7,17 +7,17 @@ class Phraseapp < Formula
   homepage 'https://phraseapp.com/cli'
   GITHUB_URL = 'https://github.com/phrase/phraseapp-client/releases/download'
 
-  version '1.15.0'
+  version '1.15.1'
   if OS.linux?
     if OS.respond_to?(:bits) && OS.bits == 32
-      sha256 'b5285e40228efa079c33aab251b5cbeb6f01cc075243a9b3249f40b3519683c3'
+      sha256 '2cee40f38deccef57254ebf3d97b68d6fde5f95306b0de15e72270f60ba604f2'
       url "#{GITHUB_URL}/#{version}/phraseapp_linux_386.tar.gz"
     else
-      sha256 'abfb82ddc99f7177c78d29951a136fc4e90e6b417154735b111d288450617ce8'
+      sha256 'cd5e44cfbe7d358d60e8fcf607b5c23ab1a5ae2d016e9fdbc07af2fab8c4cdbd'
       url "#{GITHUB_URL}/#{version}/phraseapp_linux_amd64.tar.gz"
     end
   else
-    sha256 'cc4f885945443cfc3ce7490b6e56e39810ae6db66d33f9125cacd655ecceabe7'
+    sha256 'dee73f13a3f30e13ce111eefbd8b0554b650eb854cbbebb441ff7f762f3efd50'
     url "#{GITHUB_URL}/#{version}/phraseapp_macosx_amd64.tar.gz"
   end
 
