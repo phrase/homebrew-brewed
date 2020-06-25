584bf816cb74d2096d9c34fab793c7460772eb69
Rafał Lisowski <rlisowski@users.noreply.github.com>
2020-06-25 12:39:03 +0200

    Bump phrase to 2.0.8

--- Formula/phrase.rb
+++ Formula/phrase.rb
@@ -3,17 +3,17 @@ class Phrase < Formula
   homepage "https://phrase.com/cli"
   GITHUB_URL = "https://github.com/phrase/phrase-cli/releases/download".freeze
 
-  version "2.0.7"
+  version "2.0.8"
   if OS.linux?
     if OS.respond_to?(:bits) && OS.bits == 32
-      sha256 "222322602db5dd57f9742d0ec476c5a6e0a56074f43e8350fda8e2b9344f80a6"
+      sha256 "bac2e67c329b3fbeaa97070e8658a30d12271c0e20a1b481235c953d38dc4ac3"
       url "#{GITHUB_URL}/#{version}/phrase_linux_386.tar.gz"
     else
-      sha256 "6b931d64e0b473b5b4b23cc719df2570476de7474c29e1392f014e1fe392fb4b"
+      sha256 "e8e7a2cde2a2762b1f66a93a9665559ea0353760260ea512e10a35797fea49ff"
       url "#{GITHUB_URL}/#{version}/phrase_linux_amd64.tar.gz"
     end
   else
-    sha256 "c949b42d023964ff7e29e2812ca89ea8f826ab47c55995b5763414d66659bb05"
+    sha256 "83792f09b6b0a04e3d271d58deb5220a6c66bef9618920657ba9938191f9f854"
     url "#{GITHUB_URL}/#{version}/phrase_macosx_amd64.tar.gz"
   end
 
