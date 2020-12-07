6363b7663126ecfe0afaeb5ed5c28088158db9e1
Aleksandr Lossenko <29657+egze@users.noreply.github.com>
2020-12-07 17:17:00 +0100

    release 2.0.18 (#31)

--- Formula/phrase.rb
+++ Formula/phrase.rb
@@ -3,17 +3,17 @@ class Phrase < Formula
   homepage "https://phrase.com/cli"
   GITHUB_URL = "https://github.com/phrase/phrase-cli/releases/download".freeze
 
-  version "2.0.17"
+  version "2.0.18"
   if OS.linux?
     if OS.respond_to?(:bits) && OS.bits == 32
-      sha256 "a04061d1526575b4bf2df9133eb7c28e36ea84e2bbe1039df13eaeaacfde6800"
+      sha256 "d05dd7e49e6e1cf987ab2b28dd130f88f28d5b804f5139a2328bbc96939d13a0"
       url "#{GITHUB_URL}/#{version}/phrase_linux_386.tar.gz"
     else
-      sha256 "2072bb0d51289bf7c80ca575511a531d581e6b8c6f8463a5bc6113b30e070303"
+      sha256 "a39f9bea6eda29242bb245c797fa768c1cedf13af9f4f1fa5182444e5bd43900"
       url "#{GITHUB_URL}/#{version}/phrase_linux_amd64.tar.gz"
     end
   else
-    sha256 "e3ed5028da3f3288a4f70df6573d30de690dff1ce7ce69ab5920ac00aa202070"
+    sha256 "c0de1a333785fe0e5bcaa63ec31e5629840485cb7f604abc69fe0c0e667a3eed"
     url "#{GITHUB_URL}/#{version}/phrase_macosx_amd64.tar.gz"
   end
 
