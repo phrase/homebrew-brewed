f6572ad858742dabf64c0b5f2f92d0d6fa055a45
Aleksandr Lossenko <aleksandr.lossenko@phrase.com>
2020-12-09 11:34:24 +0100

    add filenames to commenst to match them later

--- Formula/phrase.rb
+++ Formula/phrase.rb
@@ -6,14 +6,14 @@ class Phrase < Formula
   version "2.0.18"
   if OS.linux?
     if OS.respond_to?(:bits) && OS.bits == 32
-      sha256 "d05dd7e49e6e1cf987ab2b28dd130f88f28d5b804f5139a2328bbc96939d13a0"
+      sha256 "d05dd7e49e6e1cf987ab2b28dd130f88f28d5b804f5139a2328bbc96939d13a0" # phrase_linux_386.tar.gz
       url "#{GITHUB_URL}/#{version}/phrase_linux_386.tar.gz"
     else
-      sha256 "a39f9bea6eda29242bb245c797fa768c1cedf13af9f4f1fa5182444e5bd43900"
+      sha256 "a39f9bea6eda29242bb245c797fa768c1cedf13af9f4f1fa5182444e5bd43900" # phrase_linux_amd64.tar.gz
       url "#{GITHUB_URL}/#{version}/phrase_linux_amd64.tar.gz"
     end
   else
-    sha256 "c0de1a333785fe0e5bcaa63ec31e5629840485cb7f604abc69fe0c0e667a3eed"
+    sha256 "c0de1a333785fe0e5bcaa63ec31e5629840485cb7f604abc69fe0c0e667a3eed" # phrase_macosx_amd64.tar.gz
     url "#{GITHUB_URL}/#{version}/phrase_macosx_amd64.tar.gz"
   end
 
